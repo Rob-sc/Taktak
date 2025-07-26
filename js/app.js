b74f880720529b345126c8d21c6d9d44691ba717
robert <roberto_scr@outlook.com>
2025-07-26 14:47:56 +0200

    Upgrade css and js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const time = document.getElementById('time')
+const score = document.getElementById('score')
 
 let num = 0
+let score_points = 0
 
 time.innerHTML = num
+score.innerHTML = score_points
 
 function incrementar(){
     num += 1
