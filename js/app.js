a11706f4ed0260761e1b5e6427409da9665a6f2b
robert <roberto_scr@outlook.com>
2025-07-27 15:08:13 +0200

    Upgrade css and js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
-const time = document.getElementById('time')
-const score = document.getElementById('score')
+const $time = document.getElementById('time')
+const $score = document.getElementById('score')
+
 
 let num = 0
 let score_points = 0
+let TEXT_GAME = "Lore ip sum"
 
-time.innerHTML = num
-score.innerHTML = score_points
+$time.innerHTML = num
+$score.innerHTML = score_points
 
-function incrementar(){
-    num += 1
-    time.innerHTML = num
+gnr_text(TEXT_GAME)
+
+const $p = document.querySelector('p')
+
+function gnr_text(text){
+    const words = text.split(" ")
+    words.forEach(word => {
+        $p.innerHTML += "<word>" + word.split('') 
+        + "</word>"
+    });
 }
-
-//update_time(60)
